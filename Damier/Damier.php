dacd3cee0da897c987ffc793c81709f2414c9a74
Killerb17 <159477925+Killerb17@users.noreply.github.com>
2024-04-11 19:23:18 +0200

    Add files via upload

---
+++ Damier/Damier.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DAMIER</title>
+   
+</head>
+<body>
+
+<?php
+for ($i=0; $i<=10; $i++) {
+    echo "<div style='clear:both'> ";
+
+    for ($j=0;$j<=10; $j++){
+         if(($i+$j)% 2 == 0){
+            echo"<div style='width: 50px;height: 50px;
+                background-color: black; float: left'> </div>";
+            } else {
+                echo "<div style='width: 50px;height: 50px;
+                background-color: white; float: left'> </div>";
+            }
+            
+         }
+         echo "</div>";
+    }
+
+?>
+
+</body>
+</html>
+
+
